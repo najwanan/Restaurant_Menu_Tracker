a3af8a4f199ce59a41c90a42a135784506236be0
najwanan <najwanan@hotmail.com>
2022-09-04 15:37:37 -0700

    setting index controller and search methods

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,35 @@
 const schemas = require('../models/schema.js')
+
+module.exports = {
+    getHome: async (req, res) => {
+        let menu = schemas.menu
+        let sesh = req.session
+        
+        let menuResult = await menu.find({})
+            .then((menuData) => {
+            res.render('index', {title: 'Menu Tracker', data: menuData, search: '', loggedIn: sesh.loggedIn})
+        })
+    },
+
+    getSearch: async (req, res) => {
+        let menu = schemas.menu
+        let sesh = req.session
+        let q = req.body.searchInput //pull data from the form 
+        let menuData = null 
+        let qry = { name: { $regex: '^' + q }, $options: 'i' } //i refers to ignore case
+        
+        if (q != null) {
+            let menuResult = await menu.find(qry)
+                .then((data) => {
+                menuData = data
+            })
+        }
+        //render results of query to index view
+          res.render("index", {
+            title: "Menu Tracker",
+            data: menuData,
+            search: q,
+            loggedIn: sesh.loggedIn,
+          });
+    }
+}
