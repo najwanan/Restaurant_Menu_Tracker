96a5208793a51b683bf5b2f9d0e47fa238cfb423
najwanan <najwanan@hotmail.com>
2022-09-05 23:23:02 -0700

    working out bugs for controllers

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 const loginRouter = require("./routes/login");
 const menuRouter = require("./routes/menu");
-
 
 require("dotenv").config({ path: "./config/.env" });
 
@@ -41,9 +40,9 @@ app.use(
 app.use(flash());
 
 //add routes here
-app.use('/', indexRouter)
-app.use('/menu', menuRouter)
-app.use('/login', loginRouter);
+app.use("/", indexRouter);
+app.use("/menu", menuRouter);
+app.use("/login", loginRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
